b99afeba0cdf9daa9d650e8cf8e121f5f483b01a
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-07-23 22:22:36 +0200

    renaming ingredient to product

--- Heartcooking.API/Controllers/ProductsController.cs
+++ Heartcooking.API/Controllers/ProductsController.cs
@@ -5,19 +5,19 @@ namespace Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class IngredientsController : ControllerBase
+    public class ProductsController : ControllerBase
     {
         
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] {"ingredient1", "ingredient2"};
+            return new string[] {"product1", "product2"};
         }
 
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return $"ingredient with id {id}";
+            return $"product with id {id}";
         }
     }
 }
--- Heartcooking.API/Models/Product.cs
+++ Heartcooking.API/Models/Product.cs
@@ -1,6 +1,6 @@
 namespace Models
 {
-    public class Ingredient
+    public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; }
