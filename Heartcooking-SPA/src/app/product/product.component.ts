1d7d913d114b4f33637b389192f30845a141b4cf
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-07-27 17:45:55 +0200

    requesting products from api in frontend

---
+++ Heartcooking-SPA/src/app/product/product.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Product } from '../models/product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+
+  products: Product[];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+
+    this.getProducts();
+  }
+
+  getProducts(): void {
+
+    this.http.get('http://localhost:5000/api/products').subscribe((response: Product[]) => {
+      this.products = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+}
