06042ab8c2c8fae848c0f76bcf269d07f32c8548
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-07-27 22:53:46 +0200

    adding logging to HeartcookingRepository

--- Heartcooking.API/Data/HeartcookingRepository.cs
+++ Heartcooking.API/Data/HeartcookingRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Models;
 
 namespace Data
@@ -10,16 +11,19 @@ namespace Data
     public class HeartcookingRepository : IHeartcookingRepository
     {
         private readonly DataContext context;
-        
-        public HeartcookingRepository(DataContext context)
+        private readonly ILogger<HeartcookingRepository> logger;
+
+        public HeartcookingRepository(DataContext context, ILogger<HeartcookingRepository> logger)
         {
             this.context = context;
+            this.logger = logger;
         }
         
 
 
         public async Task<Product> GetProduct(int id)
         {
+            logger.LogTrace($"Fetch product with id {id}");
             Product product = await context.Products.FirstOrDefaultAsync(product => product.Id == id);
 
             return product;
@@ -27,22 +31,26 @@ namespace Data
 
         public async Task<IEnumerable<Product>> GetProducts()
         {
+            logger.LogTrace($"Fetch products");
             IEnumerable<Product> products = await context.Products.ToListAsync();;
 
             return products;
         }
         public void Add<T>(T entity) where T : class
         {
+            logger.LogTrace($"Adding entity {entity.ToString()}");
             context.Add(entity);
         }
 
         public void Delete<T>(T entity) where T : class
         {
+            logger.LogTrace($"Deletingentity {entity.ToString()}");
             context.Remove(entity);
         }
 
         public async Task<bool> SaveAll()
         {
+            logger.LogTrace($"Saving all changes");
             return await context.SaveChangesAsync() > 0;
         }
     }
