5aa06d7f97c8345b4bdca3e6cc62939febbc8817
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-07-28 22:28:10 +0200

    adding memory cache for products list

--- Heartcooking.API/Data/HeartcookingRepository.cs
+++ Heartcooking.API/Data/HeartcookingRepository.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using Models;
 
@@ -10,15 +12,19 @@ namespace Data
 {
     public class HeartcookingRepository : IHeartcookingRepository
     {
+        private readonly IMemoryCache cache;
         private readonly DataContext context;
         private readonly ILogger<HeartcookingRepository> logger;
 
-        public HeartcookingRepository(DataContext context, ILogger<HeartcookingRepository> logger)
+        private readonly string productsCacheKey = "products";
+
+        public HeartcookingRepository(DataContext context, ILogger<HeartcookingRepository> logger, IMemoryCache cache)
         {
+            this.cache = cache;
             this.context = context;
             this.logger = logger;
         }
-        
+
 
 
         public async Task<Product> GetProduct(int id)
@@ -32,7 +38,17 @@ namespace Data
         public async Task<IEnumerable<Product>> GetProducts()
         {
             logger.LogTrace($"Fetch products");
-            IEnumerable<Product> products = await context.Products.ToListAsync();;
+            IEnumerable<Product> products = cache.Get<IEnumerable<Product>>(productsCacheKey);
+            if (products == null)
+            {
+                logger.LogTrace($"Fetch products from context");
+                products = await context.Products.ToListAsync();
+                cache.Set(productsCacheKey, products, TimeSpan.FromMinutes(1));
+            }
+            else
+            {
+                logger.LogTrace($"Fetch products from cache");
+            }
 
             return products;
         }
