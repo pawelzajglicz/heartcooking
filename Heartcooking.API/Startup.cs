579fc9bf5fbaa99716ee3b3d024eee74cbc51579
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-07-29 19:59:11 +0200

    configuring Autofac

--- Heartcooking.API/Startup.cs
+++ Heartcooking.API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
 using Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -15,14 +16,17 @@ using Microsoft.Extensions.Logging;
 
 namespace Heartcooking.API
 {
+    
     public class Startup
     {
+
+        public IConfiguration Configuration { get; }
+        public IContainer Container { get; private set; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -33,6 +37,7 @@ namespace Heartcooking.API
             services.AddCors();
             services.AddMemoryCache();
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
