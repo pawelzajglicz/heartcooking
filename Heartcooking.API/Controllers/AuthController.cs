070bcfd509b53be505378da029aea50e24545956
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-08-02 18:20:32 +0200

    creating login method

--- Heartcooking.API/Controllers/AuthController.cs
+++ Heartcooking.API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Heartcooking.API.Data;
 using Heartcooking.API.Dtos;
 using Heartcooking.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Heartcooking.API.Controllers
 {
@@ -11,15 +17,18 @@ namespace Heartcooking.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository repository;
-        public AuthController(IAuthRepository repository)
+        private readonly IConfiguration configuration;
+
+        public AuthController(IAuthRepository repository, IConfiguration configuration)
         {
             this.repository = repository;
+            this.configuration = configuration;
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
-            
+
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
             if (await repository.UsernameTaken(userForRegisterDto.Username))
@@ -33,6 +42,41 @@ namespace Heartcooking.API.Controllers
             User createdUser = await repository.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLogin)
+        {
+            User userFromRepo = await repository.Login(userForLogin.Username.ToLower(), userForLogin.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(configuration.GetSection("AppSettings:Token").Value));
+
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = credentials
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new {
+                token = tokenHandler.WriteToken(token)
+            });
         }
     }
 }
