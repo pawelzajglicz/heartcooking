ebdde8605ae55d46ced7f6bd89d6badda88f8aa8
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-08-31 22:19:14 +0200

    adding using login method in nav component

--- Heartcooking-SPA/src/app/nav/nav.component.ts
+++ Heartcooking-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserForLogin } from './../models/user-for-login';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,13 +11,18 @@ export class NavComponent implements OnInit {
 
   userForLogin: UserForLogin = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   login() {
-    console.log(this.userForLogin);
+
+    this.authService.login(this.userForLogin).subscribe(next => {
+      console.log('Logged in successfully');
+    }, error => {
+      console.log('Failed to login');
+    });
   }
 
 }
