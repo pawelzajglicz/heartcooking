60214a0c1c1f03e151abf0a07e2489d4b1ce900a
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-09-06 22:06:11 +0200

    adding button to hide under-construction-panel

--- Heartcooking-SPA/src/app/under-construction/under-construction.component.ts
+++ Heartcooking-SPA/src/app/under-construction/under-construction.component.ts
@@ -1,11 +1,40 @@
+import { trigger, transition, style, animate, state } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { NONE_TYPE } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-under-construction',
   templateUrl: './under-construction.component.html',
-  styleUrls: ['./under-construction.component.css']
+  styleUrls: ['./under-construction.component.scss'],
+  animations: [
+        trigger('hideButton', [
+          state('hide', style({
+            height: 0,
+            'border-style': 'unset',
+            'font-size': 0,
+            'padding-top': 0,
+            'padding-bottom': 0
+          }))
+        ]),
+        trigger('heightHide', [
+            state('hide', style({
+                height: 0,
+                'padding-top': 0,
+                'padding-bottom': 0
+            })),
+            transition('* => *', animate(750))
+        ])
+    ]
 })
 export class UnderConstructionComponent implements OnInit {
+
+  state = 'visible';
+
+  changeVisibility(): void {
+    console.log(this.state);
+    (this.state === 'hide') ? this.state = 'visible' : this.state = 'hide';
+    console.log(this.state);
+  }
 
   constructor() { }
 
