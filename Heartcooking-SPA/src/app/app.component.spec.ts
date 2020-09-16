199f1791675865d94e09e09ac0137b61d27f8988
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-09-16 19:13:13 +0200

    adding missing imports to frontend tests

--- Heartcooking-SPA/src/app/app.component.spec.ts
+++ Heartcooking-SPA/src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, async } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
@@ -23,6 +24,7 @@ describe('AppComponent', () => {
         UnderConstructionComponent
       ],
       imports: [
+        BrowserAnimationsModule,
         FormsModule,
         HttpClientTestingModule,
         MatMenuModule,
--- Heartcooking-SPA/src/app/under-construction/under-construction.component.spec.ts
+++ Heartcooking-SPA/src/app/under-construction/under-construction.component.spec.ts
@@ -1,6 +1,7 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -14,6 +15,7 @@ describe('UnderConstructionComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ UnderConstructionComponent ],
       imports: [
+        BrowserAnimationsModule,
         HttpClientTestingModule
       ]
     })
