d58556d7e2c1269ff096ec998b00cdf88ef5109b
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-09-30 21:30:27 +0200

    adding missing imports to env.service tests

--- Heartcooking-SPA/src/app/services/env-config.service.spec.ts
+++ Heartcooking-SPA/src/app/services/env-config.service.spec.ts
@@ -1,11 +1,15 @@
 /* tslint:disable:no-unused-variable */
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, async, inject } from '@angular/core/testing';
 import { EnvConfigService } from './env-config.service';
 
 describe('Service: EnvConfig', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+       ],
       providers: [EnvConfigService]
     });
   });
