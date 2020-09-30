7564d76925f107c67a48b6392028e0a201031e6b
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-09-30 21:26:47 +0200

    fixing bug with empty error alertify message

--- Heartcooking-SPA/src/app/services/error.interceptor.ts
+++ Heartcooking-SPA/src/app/services/error.interceptor.ts
@@ -22,9 +22,13 @@ export class ErrorInterceptor implements HttpInterceptor {
           }
         }
 
-        const serverError = error.error;
+        let serverError = null;
+        if (serverError) {
+          serverError = error.error;
+        }
+
         let modalStateErrors = '';
-        if (serverError.errors && typeof serverError.errors === 'object') {
+        if (serverError && serverError.errors && typeof serverError.errors === 'object') {
           for (const key in serverError.errors) {
             if (serverError.errors[key]) {
               modalStateErrors += serverError.errors[key] + '\n';
