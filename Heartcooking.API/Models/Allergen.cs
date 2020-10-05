5b5895192b5f6620eca0e24188b4255ae9f749c5
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-10-05 21:58:34 +0200

    adding products collection to allergens

--- Heartcooking.API/Models/Allergen.cs
+++ Heartcooking.API/Models/Allergen.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+
 namespace Heartcooking.API.Models
 {
     public class Allergen : BaseEntity
     {
         public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
     }
 }
