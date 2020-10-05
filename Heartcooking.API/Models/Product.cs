d50a22f7ceeb5dff75d0c862d8b118927e28397a
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-10-05 21:44:45 +0200

    extending product model

--- Heartcooking.API/Models/Product.cs
+++ Heartcooking.API/Models/Product.cs
@@ -1,8 +1,17 @@
 namespace Heartcooking.API.Models
 {
-    public class Product
+    public class Product : BaseEntity
     {
-        public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+        public double Carbohydrates { get; set; }
+        public double Fat { get; set; }
+        public double Fiber { get; set; }
+        public double Kilocalories { get; set; }
+        public double Protein { get; set; }
+        public double Salt { get; set; }
+        public double SaturatedFat { get; set; }
+        public double Sugar { get; set; }
+        public bool IsVegan { get; set; }
     }
 }
