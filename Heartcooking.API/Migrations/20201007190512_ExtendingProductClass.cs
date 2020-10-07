cc7bc405631d91fba8d13b338e5e8cb4a7970ce1
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-10-07 21:08:29 +0200

    adding extending product migration

---
+++ Heartcooking.API/Migrations/20201007190512_ExtendingProductClass.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Heartcooking.API.Migrations
+{
+    public partial class ExtendingProductClass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Carbohydrates",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Products",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Fat",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Fiber",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVegan",
+                table: "Products",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Kilocalories",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Protein",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Salt",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "SaturatedFat",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Sugar",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.CreateTable(
+                name: "Allergens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Allergens", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Url = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    DateAdded = table.Column<DateTime>(nullable: false),
+                    IsMain = table.Column<bool>(nullable: false),
+                    ProductId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Photos_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductsAllergens",
+                columns: table => new
+                {
+                    AllergenId = table.Column<int>(nullable: false),
+                    ProductId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductsAllergens", x => new { x.AllergenId, x.ProductId });
+                    table.ForeignKey(
+                        name: "FK_ProductsAllergens_Allergens_AllergenId",
+                        column: x => x.AllergenId,
+                        principalTable: "Allergens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductsAllergens_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photos_ProductId",
+                table: "Photos",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductsAllergens_ProductId",
+                table: "ProductsAllergens",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Photos");
+
+            migrationBuilder.DropTable(
+                name: "ProductsAllergens");
+
+            migrationBuilder.DropTable(
+                name: "Allergens");
+
+            migrationBuilder.DropColumn(
+                name: "Carbohydrates",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Fat",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Fiber",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "IsVegan",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Kilocalories",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Protein",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Salt",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "SaturatedFat",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Sugar",
+                table: "Products");
+        }
+    }
+}
