aa48881a5609c8bcf8737182d5886938dfffdcb1
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2020-11-02 22:46:45 +0100

    fixing frontend tests

--- Heartcooking-SPA/src/app/products/products-list/products-list.component.spec.ts
+++ Heartcooking-SPA/src/app/products/products-list/products-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { ProductCardComponent } from './../product-card/product-card.component';
 import { ProductsListComponent } from './products-list.component';
 
 describe('ProductsListComponent', () => {
@@ -9,7 +10,10 @@ describe('ProductsListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductsListComponent ],
+      declarations: [
+        ProductCardComponent,
+        ProductsListComponent
+      ],
       imports: [ HttpClientTestingModule ]
     })
     .compileComponents();
